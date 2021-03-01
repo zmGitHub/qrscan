eaec4fd6a48796ae037f6dd1258bc6df969a21ae
荆学涛 <me@jingxuetao.com>
2021-03-01 10:29:46 +0800

    update

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/lihaoyu/dev/sdk/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/lihaoyu/dev/work/flutter/plugin/qrscan/example"
+export "FLUTTER_ROOT=/Users/swifter/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/swifter/Documents/code/qrscan/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/lihaoyu/dev/sdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/swifter/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.2.17"
 export "FLUTTER_BUILD_NUMBER=0.2.17"
 export "DART_OBFUSCATION=false"
